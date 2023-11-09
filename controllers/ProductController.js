543dfb6965a64959109dbb923191e4716fcb0db3
Tiago <trsminfo@gmail.com>
2023-11-09 13:21:23 -0300

    finalizado o projeto com MongoDB

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -27,9 +27,37 @@ module.exports = class ProductController {
 
     static async showProduct(req, res) {
         const id = req.params.id
-       
+
 
         const product = await Products.getProductsById(id)
         res.render('products/product', { product })
+    }
+
+    static async removeProduct(req, res) {
+        const id = req.params.id
+
+        await Products.removeProductById(id)
+        res.redirect('/products')
+    }
+
+    static async editProduct(req, res) {
+        const id = req.params.id
+
+        const product = await Products.getProductsById(id)
+
+        res.render('products/edit', { product })
+    }
+
+    static async editProductPost(req, res){
+        const id = req.body.id
+        const name = req.body.name
+        const image = req.body.image
+        const price = req.body.price
+        const description = req.body.description
+
+        const product = new Products(name, image, price, description)
+         product.updateProduct(id)
+
+        res.redirect('/products')
     }
 }
--- models/Product.js
+++ models/Product.js
@@ -31,6 +31,19 @@ class Product {
         const product = await conn.db().collection('products').findOne({ _id: new ObjectId(id) })
         return product
     }
+
+    static async removeProductById(id) {
+        
+        await conn.db().collection('products').deleteOne({ _id: new ObjectId(id) })
+
+        return
+    }
+
+     updateProduct(id){
+         conn.db().collection('products').updateOne({_id: new ObjectId(id)},{$set: this})
+
+        return
+    }
 }
 
 module.exports = Product
